3d67fe74b1e1490ca2591966a314768ce0c36e10
NguyenNgocDoan <91644802+NguyenNgocDoan@users.noreply.github.com>
2023-01-04 11:24:56 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+
+function myFunction(){
+    let carName = "Volvo"
+    document.getElementById("demo-function-1").innerHTML = typeof carName + " " + carName;
+}
+    myFunction();
+
+    document.getElementById("demo-function-2").innerHTML = typeof carName;
+
+
+// Event
+function displayDate() {
+    document.getElementById("demo-event-3").innerHTML = Date();
+}
+
+
